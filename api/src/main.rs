d848a6af07f0aaf3517f8f6cd4ea25c9306a14fb
Dominik Winecki <dominikwinecki@gmail.com>
2018-03-24 16:17:45 -0400

    Add image retrival

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,15 +3,18 @@
 
 extern crate rocket;
 extern crate redis;
+extern crate tempfile;
 
 use redis::Commands;
 use std::env;
+use std::fs::File;
+use std::io::*;
 
 const WIDTH: usize = 8;
 const HEIGHT: usize = 8;
 
 #[get("/write/<x>/<y>/<color>")]
-fn index(x: u16, y: u16, color: u8) -> &'static str {
+fn write(x: u16, y: u16, color: u8) -> &'static str {
         if color > 15 {
             return "Invalid color";
         }
@@ -33,6 +36,45 @@ fn index(x: u16, y: u16, color: u8) -> &'static str {
 
         "Success"
 }
+
+fn get_color(c: u8) -> (u8, u8, u8) {
+    match c {
+        0 => (255,255,255),  // white
+        1 => (230,126,34),   // orange
+        2 => (176,58,46),    // red
+        3 => (244,208,63),   // yellow
+        4 => (52,152,219),   // blue
+        5 => (82,190,128),   // green
+        6 => (31,97,141),    // dark blue
+        7 => (125,60,152),   // purple
+        8 => (233,30,99),    // pink
+        9 => (195,155,211),  // lavender
+        10 => (118,215,196), // teal
+        11 => (214,234,248), // light blue
+        12 => (210,180,140), // tan
+        13 => (23,32,42),    // black
+        14 => (129,199,132), // light green
+        15 => (160,64,0),    // brown
+        _ => panic!()
+    }
+} 
+
+#[get("/board.png")]
+fn board() -> String {
+    let board = get_board();
+    let size = 12;
+    let mut content = String::new();
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let (r,g,b) = get_color(board[x as usize + y as usize * WIDTH]);
+            content += &format!("<rect x=\"{x}\" y = \"{y}\" width = \"{size}\" height = \"{size}\" style=\"fill:rgb({r},{g},{b});\" />", x = x * size, y = y * size, size = size, r=r, g=g,b=b);
+        }
+    }
+    format!("<svg width = \"{w}\" height = \"{h}\">
+                {content} 
+            </svg>", content = content, w = WIDTH * size, h = HEIGHT * size)
+}
+
 
 fn get_con() -> redis::Connection {
  let client = redis::Client::open(&env::var("REDIS").unwrap() as &str).unwrap();
@@ -59,7 +101,7 @@ fn main() {
     let _: () = con.set("board", empty_board).unwrap();
     
 
-            rocket::ignite().mount("/", routes![index]).launch();
+            rocket::ignite().mount("/", routes![write,board]).launch();
 
 }
 
